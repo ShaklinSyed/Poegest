1757a0b2602a87b4218ffc251457b87e620cf8a8
ShaklinSyed <shaklin_s@yahoo.com>
2014-09-08 16:57:59 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+echo <<<_END
+
+<!Doctype html>
+
+<html>
+	<head>
+		<title>Poegest</title>
+	</head>
+
+	<body>
+		<span>Poegest</span>
+		<form>
+			<input type="text" />
+			<br />
+			<input type="button" value= "search" />
+		</form>
+	</body>
+</html>
+
+_END;
+?>
+
